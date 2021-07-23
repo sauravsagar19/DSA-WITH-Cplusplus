af7a6fa7e912d935142795cca924b5d161c0c367
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-07-23 22:49:56 +0530

    Add files via upload

---
+++ LOVE BABBAR 450/arraySort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+int arr[5]={0,0,1,0,2};
+// sort(arr,arr+5);
+// for(int i=0;i<5;i++){
+//     cout<<arr[i]<<" ";
+// }
+for(int i=0;i<5;i++){
+    
+}
+
+return 0;
+}
