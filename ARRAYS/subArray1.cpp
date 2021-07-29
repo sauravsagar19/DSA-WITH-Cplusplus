c9ba4010c197b69179fa02f143c04be643f75f5e
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-07-30 01:43:11 +0530

    Add files via upload

---
+++ ARRAYS/subArray1.cpp
@@ -0,0 +1,40 @@
+// SubArray with given sum
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int n, val,s;
+    cin>>n>>s;
+    int arr[n];
+    for(int i=0;i<n;i++){
+    cin>>val;
+    arr[i]=val;
+    }
+    int i=0,j=0,st=-1,en=-1;
+    int sum=0;
+    while(j<n && sum+arr[j]<=s){
+        sum+=arr[j];
+        j++;
+    }
+    if(sum==s){
+        cout<<i+1<<" "<<j<<endl;
+        return 0;
+    }
+    while(j<n){
+        sum+=arr[j];
+        while(sum>s){
+            sum-=arr[i];
+            i++;
+        }
+        if(sum==s){
+            st=i+1;
+            en=j+1;
+            break;
+        }
+        j++;
+    }
+
+    cout<<st<<" "<<en<<endl;
+return 0;  
+} // namespace std;
