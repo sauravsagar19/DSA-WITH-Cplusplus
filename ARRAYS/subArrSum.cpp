973e16af66415877215dda6c90fbf3279e9025c0
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-07-31 02:42:07 +0530

    Add files via upload

--- ARRAYS/subArrSum.cpp
+++ ARRAYS/subArrSum.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 int main(){
-int arr[4]={1,2,3};
+int arr[5]={1,-4,3,2,1};
 int sum=0;
-for(int i=0;i<3;i++){
+int max_sum= INT_MIN;
+for(int i=0;i<5;i++){
     sum=0;
-    for(int j=i;j<3;j++){
+    for(int j=i;j<5;j++){          // Time complexity: O(n^2)
         sum+=arr[j];
-        cout<<sum<<" ";
+        // cout<<sum<<" ";
+        
     }
+    max_sum=max(max_sum,sum);
     cout<<endl;
 }
+cout<<"Maximum Sum"<<max_sum;
 return 0;
 }
