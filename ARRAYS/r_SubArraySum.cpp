dad592e12bb098a18cdb2250514675a6c7ee84b6
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-08-12 22:58:53 +0530

    Add files via upload

---
+++ ARRAYS/r_SubArraySum.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<cmath>
+#include<limits>
+using namespace std;
+int main(){
+int n,s;
+cin>>n>>s;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+
+int i=0,j=0,st=-1,en=-1;
+int sum=0;
+
+while(j<n && sum+arr[j]<=s){
+    sum+=arr[j];
+    j++;
+}
+
+if(sum==s){
+    cout<<i+1<<" "<<j<<endl;
+}
+else{
+    while(j<n){
+        sum+=arr[j];
+        while(sum>s){
+            sum-=arr[i];
+            i++;
+        }
+        if(sum==s){
+            st=i+1;
+            en=j+1;
+            break;
+        }
+        j++;
+    }
+}
+cout<<st<<"  "<<en<<endl;
+
+return 0;
+}
+
