98168c8645df7e615480daa71e7616bd976c44d1
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-08-17 14:55:47 +0530

    Add files via upload

---
+++ ARRAYS/Sieve_of_Eratosthenes2.cpp
@@ -0,0 +1,36 @@
+// prime factorisation using Sieve of Eratosthenes
+
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<cmath>
+#include<limits>
+using namespace std;
+
+void primeFactors(int n){
+    int spf[n+1]={0};
+    for(int i=2;i<=n;i++){
+        spf[i]=i;
+}
+
+for(int i=2;i<=n;i++){
+    if(spf[i]==i){
+        for(int j=i*i;j<=n;j+=i){
+            if(spf[j]==j){
+                spf[j]=i;
+            }
+        }
+    }
+}
+
+while(n!=1){
+    cout<<spf[n]<<" ";
+    n=n/spf[n];
+}
+}
+int main(){
+    int n;
+    cin>>n;
+primeFactors(n);
+return 0;
+}
