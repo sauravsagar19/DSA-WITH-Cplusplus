18ac60918208764f3e9d60a177d76dfcb7bc1692
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-10-02 02:24:19 +0530

    Add files via upload

---
+++ STACK/ReverseSentence.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void ReverseString(string s){
+    stack<string>st;
+    for(int i=0;i<s.length();i++){
+        string word="";
+        while(s[i]!=' ' && i<s.length()){
+            word+=s[i];
+            i++;
+        }
+        st.push(word);
+    }
+
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }cout<<endl;
+
+}
+int main(){
+string s="How are you?";
+ReverseString(s);
+return 0;
+}
