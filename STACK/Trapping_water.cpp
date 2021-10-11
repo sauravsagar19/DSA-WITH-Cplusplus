62ce6d2df0952c4d7fea0cb705af6156a7bb1afe
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-10-12 02:36:32 +0530

    Add files via upload

--- STACK/Trapping_water.cpp
+++ STACK/Trapping_water.cpp
@@ -1,15 +1,17 @@
+// Using Stack
+
 #include<iostream>
 #include<algorithm>
 #include<string>
 #include<cmath>
-#include<limits>
-#include<vector>
 #include<stack>
+#include<vector>
+#include<limits>
 using namespace std;
 int rain_water(vector<int>a){
     stack<int>st;
+    int area;
     int n=a.size();
-    int ans;
     for(int i=0;i<n;i++){
         while(!st.empty() && a[st.top()]<a[i]){
             int curr=st.top();
@@ -17,13 +19,13 @@ int rain_water(vector<int>a){
             if(st.empty()){
                 break;
             }
-            int len= i-st.top()-1;
-            ans+= (min(a[st.top()],a[i])-a[curr])*len;
-
+            int len=i-st.top()-1;
+            area+=(min(a[st.top()],a[i])-a[curr])  * len;
+                        // height                    length(diff)
         }
         st.push(i);
     }
-    return ans;
+    return area;
 }
 int main(){
 vector<int>a={0,1,0,2,1,0,1,3,2,1,2,1};
