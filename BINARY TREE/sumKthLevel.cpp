2ddad03951bce4504e986bde9efc5e61582a6421
SAURAV SAGAR <64888744+sauravsagar19@users.noreply.github.com>
2021-11-03 01:06:11 +0530

    Add files via upload

--- BINARY TREE/sumKthLevel.cpp
+++ BINARY TREE/sumKthLevel.cpp
@@ -52,10 +52,6 @@ int main(){
 struct node * root= new node (1);
 root->left=new node (2);
 root->right=new node(3);
-/* 
-        1
-    2       3
-*/
 root->left->left=new node(4);
 root->left->right=new node(5);
 root->right->left=new node(6);
